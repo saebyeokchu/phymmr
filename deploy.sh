9ee68a7358395678d35d3e07b81963db0fa44ebb
saebyeokchu <cuu2252@gmail.com>
2024-05-12 21:40:14 +0900

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "> PHYMMR 배포"
-sudo npm install next@latest react@latest react-dom@latest
-sudo npx next start
+npm install next@latest react@latest react-dom@latest
+npx next start
