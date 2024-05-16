d6b7ce257f9506b005cba1874a876309cc301303
saebyeokchu <cuu2252@gmail.com>
2024-05-16 09:18:32 +0900

    edit appspect timeout for after install and try to run npm install

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 echo "> PHYMMR 배포"
 sudo su
-cat deploy.sh > deploy-test.txt
-#npm install next@latest react@latest react-dom@latest
+npm install next@latest react@latest react-dom@latest
 #npx next start
