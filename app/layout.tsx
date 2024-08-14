9f139f165c357d8de9c5514c91d52aa383efc149
saebyeok chu <cuu2252@gmail.com>
2024-08-14 13:11:54 +0900

    [editor] v20240814.02

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-white`}>
-        <Middle>
+        <Middle> 
           {children}
         </Middle>
       </body>
